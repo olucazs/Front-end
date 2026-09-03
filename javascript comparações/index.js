95f1c5bcaac59b2f04beae60c7844f0d1aa6045a
olucazs <ulucas018@gmail.com>
2026-09-03 15:57:10 -0300

    javascript
    practicing and learning more about JS

---
+++ javascript comparações/index.js
@@ -0,0 +1,8 @@
+let lucas = 20;
+let alison = 28;
+
+if (lucas > alison) {
+    console.log("Lucas é mais velho que o Alison.");
+}else {
+    console.log("Alison é mais velho que o Lucas.");
+}
---
+++ javascript operadores/index.js
@@ -0,0 +1,36 @@
+// OPERADORES
+
+let a = 15;
+let b = 5;
+
+// Este código soma os números e imprime o resultado no console
+let sum = a + b;
+console.log(`The sum of ${a} and ${b} is ${sum}.`);
+
+// Este código multiplica os números e imprime o resultado no console
+let multiplication = a * b;
+console.log(`The multiplication of ${a} and ${b} is ${multiplication}.`);
+
+// Este código divide os números e imprime o resultado no console
+let division = a / b;
+console.log(`The division of ${a} by ${b} is ${division}.`);
+
+// Este código subtrai os números e imprime o resultado no console
+let subtraction = a - b;
+console.log(`The subtraction of ${b} from ${a} is ${subtraction}.`);
+
+// Este código exponencia os números e imprime o resultado no console
+let exponentiation = a ** b;
+console.log(`The exponentiation of ${a} to the power of ${b} is ${exponentiation}.`);
+
+// Este código incrementa os números e imprime o resultado no console
+let increment = a++;
+console.log(`The increment of ${a} is ${increment}.`);
+
+// Este código decrementa os números e imprime o resultado no console
+let decrement = b--;
+console.log(`The decrement of ${b} is ${decrement}.`);
+
+// Este código calcula o resto da divisão dos números e imprime o resultado no console
+let remainder = a % b;
+console.log(`The remainder of the division of ${a} by ${b} is ${remainder}.`);
