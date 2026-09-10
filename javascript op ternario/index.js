66228ec2f861842c6d34cfd26fca6f12015200e1
olucazs <ulucas018@gmail.com>
2026-09-10 13:39:00 -0300

    javascript
    operadores ternarios

---
+++ javascript op ternario/index.js
@@ -0,0 +1,17 @@
+let port = 10;
+let mat = 10;
+let art = 8;
+
+let sum = port + mat + art;
+
+let media = sum / 3;
+
+
+let resultado = media >= 5 ? 'Aprovado' : 'Reprovado'
+console.log(resultado)
+
+//=========================================================
+
+let idade = 18;
+let res = idade >= 18 ? 'Maior de idade' : 'Menor de idade'
+console.log(res)
